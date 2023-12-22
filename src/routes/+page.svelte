fa6e7b8922792acd5f44a4edcb37b1ffc92bd219
hlafaille <hlafaille@proton.me>
2023-12-22 17:03:40 -0500

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Card from './components/Card.svelte';
+</script>
+
+<html lang="en">
+	<Card title="Hello World">
+		<p>Poop</p>
+	</Card>
+</html>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
