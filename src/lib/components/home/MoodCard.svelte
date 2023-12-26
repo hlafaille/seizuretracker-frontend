3c834072ad277f77afc31836446f52367d96d6a4
hlafaille <hlafaille@proton.me>
2023-12-26 16:26:05 -0500

    #4 adding base stuff for mood survey

--- src/lib/components/home/MoodCard.svelte
+++ src/lib/components/home/MoodCard.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
 	import Card from '$lib/components/tk/cards/Card.svelte';
 	import Button from '$lib/components/tk/buttons/Button.svelte';
+	import { sendPostRequest } from '$lib/utils/requestHandler/requestSender';
+
+	/**
+	 * todo add an onMount & GET call to check if it's been X amount of time since last survey entry
+	 */
+
+	/**
+	 * PROPS
+	 */
+	export let errorMessage: string | null = null;
+	export let isLoading: boolean = false;
+
+	/**
+	 * ENUMS
+	 */
+	enum MoodSurveyAllowedMoods {
+		BAD = 'BAD',
+		CONFUSED = 'CONFUSED',
+		MEH = 'MEH',
+		GOOD = 'GOOD'
+	}
+
+	/**
+	 * Sets
+	 * @param mood
+	 */
+	async function doMoodSurveyEntrySubmission(mood: MoodSurveyAllowedMoods) {
+	}
+
 </script>
 
 <Card title="How are you feeling?" subtitle="We'll generate a graph soon.">
