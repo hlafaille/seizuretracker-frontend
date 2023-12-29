730d391a0f29a59cf6c03c0bf116c4498e36b3c7
hlafaille <hlafaille@proton.me>
2023-12-29 14:24:02 -0500

    #10 changing GlobalConstant.ts to use runtime environment variables

--- src/lib/utils/GlobalConstant.ts
+++ src/lib/utils/GlobalConstant.ts
@@ -2,4 +2,4 @@ import { RequestFactory } from '$lib/utils/requesthandler/RequestFactory';
 import { env } from '$env/dynamic/public';
 
 
-export const REQUEST_FACTORY: RequestFactory = new RequestFactory(`${env.PUBLIC_API_URL}`)
+export const REQUEST_FACTORY: RequestFactory = new RequestFactory(`${env.PUBLIC_API_URL}/v1`)
