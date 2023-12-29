0f41686da9339332d49b66983741380fbe7bdc17
hlafaille <hlafaille@proton.me>
2023-12-29 14:15:36 -0500

    #10 changing GlobalConstant.ts to use PUBLIC env vars

--- src/lib/utils/GlobalConstant.ts
+++ src/lib/utils/GlobalConstant.ts
@@ -1,4 +1,5 @@
 import { RequestFactory } from '$lib/utils/requesthandler/RequestFactory';
+import { PUBLIC_API_URL } from '$env/static/public'
 
-const apiUrl = import.meta.env.VITE_API_URL;
-export const REQUEST_FACTORY: RequestFactory = new RequestFactory(`${apiUrl}`)
+
+export const REQUEST_FACTORY: RequestFactory = new RequestFactory(`${PUBLIC_API_URL}`)
