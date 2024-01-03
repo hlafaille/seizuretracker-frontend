cd6bb67a186a27bd2c77b52dcbe08da1e019bfcf
hlafaille <hlafaille@proton.me>
2024-01-02 22:44:57 -0500

    #1 added redirect to ensure a session cookie exists

--- src/lib/utils/requesthandler/HeaderHelper.ts
+++ src/lib/utils/requesthandler/HeaderHelper.ts
@@ -1,4 +1,7 @@
 import { getCookie } from '$lib/utils/CookieHandler';
+import { STORE_AUTH_LOGIN_ERROR_MESSAGE } from '$lib/utils/GlobalStore';
+import { goto } from '$app/navigation';
+import { SessionCookieMissingError } from '$lib/utils/requesthandler/SessionCookieMissingError';
 
 /**
  * Get an object of headers to send
@@ -14,7 +17,9 @@ export function getHeaders(authHeader = true) {
 		if (typeof sessionCookieValue == 'string') {
 			requestHeaders.Authorization = `Bearer ${getCookie('session')!}`;
 		} else {
-			console.debug("session cookie value was null, ignoring")
+			STORE_AUTH_LOGIN_ERROR_MESSAGE.set("No session was detected for your device. Please log in.")
+			goto("/auth/login")
+			throw new SessionCookieMissingError();
 		}
 	}
 	return requestHeaders;
